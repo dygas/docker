cc2c89c63b209bcc17babb19cd0a11f5fcd03ad6
Grzegorz Dygas <grzegorz.dygas@hotmail.com>
2020-06-11 00:47:38 +0200

    django

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,6 +16,13 @@ class ModelTests(TestCase):
         self.assertEqual(user.email,email)
         self.assertTrue(user.check_password(password))
 
+    def test_new_user_email_normalized(self):
+        """Test the mail for new user is normalized"""
+        email = 'test@LONDONAPPDEV.COM'
+        user =get_user_model().object.create_user(email, 'test123')
+
+        self.assertEqual(user.email,email.lower())
+
 
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
